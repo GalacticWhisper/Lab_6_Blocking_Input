1d4699d564f175e2f575de23383e8abf4ba9851e
IanM <ianmarr@Ians-MacBook-Air.local>
2024-10-04 16:19:37 -0400

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double userInputC = 0;
+        double userInputF = 0;
+        boolean done = false;
+        String trash;
+
+            do {
+                System.out.println("Enter a temperature in degrees Celsius: ");
+                if (in.hasNextDouble())
+                {
+                    userInputC = in.nextDouble();
+                    userInputF = userInputC * 1.8 + 32;
+                    done = true;
+                }
+                else
+                {
+                    trash = in.nextLine();
+                    System.out.println("The input you provided was " + trash + ", which is invalid. Please enter a number.");
+                }
+            } while (!done);
+            System.out.println("The value you entered was " + userInputC + ". In degrees Fahrenheit, your value is now " + userInputF + ".");
+        }
+    }
